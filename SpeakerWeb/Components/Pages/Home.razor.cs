094f8618ec7c75644a73c3688437598774382872
deveb1479@gmail.com <deveb1479@gmail.com>
2024-05-28 13:53:14 +0900

    temp

---
+++ SpeakerWeb/Components/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SpeakerWeb.Components.Pages
+{
+    public partial class Home
+    {
+        public string Title { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+
+        private void SubmitButtonClick(MouseEventArgs e)
+        {
+
+        }
+    }
+}
