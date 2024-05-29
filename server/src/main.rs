6627c98a06ed6d4350b6998432fcc6e702904082
elecbug <deveb1479@gmail.com>
2024-05-29 19:08:13 +0900

    yeah

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,11 +5,11 @@ use chrono::Local;
 use tower_http::cors::CorsLayer;
 
 mod structs;
-use structs::{SubmitArgs, RoundSubmit};
+use structs::{RoundSubmit, SubmitArgs, SubmitWithTime};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let ip = "localhost:80";
+    let ip = "localhost:8080";
 
     let cors = CorsLayer::new()
         .allow_origin(ip.parse::<HeaderValue>()?)
@@ -49,12 +49,19 @@ fn create_router() -> Router {
         .route("/api/health", routing::get(health_handler))
         .route("/api/submit", routing::get(submit_screen_handler))
         .route("/api/submit/:args", routing::get(submit_handler))
+        .route("/api/cancel/:args", routing::get(submit_cancel_handler))
+}
+
+fn error_html() -> Html<String> {
+    Html::<String>(include_str!("static/error.html").to_string())
 }
 
 async fn health_handler() -> impl IntoResponse {
     println!("Who check server health");
 
-    let html = format!(include_str!("static/alive.html"), Local::now().to_string());
+    let html = format!(
+        include_str!("static/alive.html"),
+        Local::now().to_string());
 
     Html::<String>(html)
 }
@@ -65,33 +72,87 @@ async fn submit_screen_handler() -> impl IntoResponse {
     Html::<String>(html)
 }
 
-async fn submit_handler(Form(args): Form<SubmitArgs>) -> impl IntoResponse {
-    println!("Title: {}\nName: {}\nDescription: {}", args.title, args.name, args.description);
+async fn submit_handler(Form(args): Form<SubmitArgs>)
+    -> impl IntoResponse {
+    println!("#Add\nTitle: {}\nName: {}\nDescription: {}",
+        args.title, args.name, args.description);
 
-    let html = format!(include_str!("static/success.html"), args.title, args.name, args.description);
+    let date: String = Local::now().format("%Y%m%d").to_string();
+    let time = Local::now().format("%H%M%S%f.rqj").to_string();
+
+    let html = format!(include_str!("static/success.html"),
+        args.title, args.name, args.description, &date, &time,
+        args.title, args.name, args.description, &date, &time);
 
     if path::Path::new("./submits/").exists() == false {
         match fs::create_dir(path::Path::new("./submits/")) {
             Ok(_) => {},
-            Err(_) => return Html::<String>("Error".to_string()),
+            Err(_) => return error_html(),
+        };
+    }
+    
+    let daily = format!("./submits/{}", &date);
+    let daily = daily.as_str();
+
+    if path::Path::new(daily).exists() == false {
+        match fs::create_dir(path::Path::new(daily)) {
+            Ok(_) => {},
+            Err(_) => return error_html(),
         };
     }
 
-    let file = Local::now().format("%Y%m%d%H%M%S%f.rqj").to_string();
-    let file = format!("./submits/{}", file);
+    let file = format!("./submits/{}/{}", &date, &time);
     let mut file = match File::create(path::Path::new(&file)) {
         Ok(o) => o,
-        Err(_) => return Html::<String>("Error".to_string()),
+        Err(_) => return error_html(),
     };
 
-    let args = match serde_json::to_string(&RoundSubmit {submit: args, rr: 0}) {
+    let args = match serde_json::to_string(&RoundSubmit {
+        submit: args,
+        rr: 0,
+    }) {
         Ok(o) => o,
-        Err(_) => return Html::<String>("Error".to_string()),
+        Err(_) => return error_html(),
     };
 
     match file.write(args.as_bytes()) {
         Ok(_) => {},
-        Err(_) => return Html::<String>("Error".to_string()),
+        Err(_) => return error_html(),
+    };
+
+    Html::<String>(html)
+}
+
+async fn submit_cancel_handler(Form(args): Form<SubmitWithTime>)
+    -> impl IntoResponse {
+    println!("#remove\nTitle: {}\nName: {}\nDescription: {}",
+        args.title, args.name, args.description);
+
+    let html = format!(include_str!("static/cancel.html"),
+        args.title, args.name, args.description);
+
+    if path::Path::new("./submits/").exists() == false {
+        match fs::create_dir(path::Path::new("./submits/")) {
+            Ok(_) => {},
+            Err(_) => return error_html(),
+        };
+    }
+
+    let daily = format!("./submits/{}", &args.date);
+    let daily = daily.as_str();
+
+    if path::Path::new(daily).exists() == false {
+        match fs::create_dir(path::Path::new(daily)) {
+            Ok(_) => {},
+            Err(_) => return error_html(),
+        };
+    }
+
+    let file = format!("./submits/{}/{}", &args.date, &args.time);
+
+    match fs::remove_file(file) {
+        Ok(_) => {},
+        Err(_) => return error_html(),
     };
 
     Html::<String>(html)
--- server/src/structs.rs
+++ server/src/structs.rs
@@ -13,3 +13,12 @@ pub struct RoundSubmit {
     pub submit: SubmitArgs,
     pub rr: i32,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct SubmitWithTime {
+    pub title: String,
+    pub name: String,
+    pub description: String,
+    pub date: String,
+    pub time: String,
+}
